bd8dcf6784eaed3cf5219d270a47f21b5f7b532c
Elfo <renanmedeirosaguiar@gmail.com>
2025-08-25 15:38:02 -0300

    Fixed age calculation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import pandas as pd
 
+AVARAGE_DAYS_IN_A_YEAR = 365.2425
 
 def unique_ids(data):
     players_ids = data["playerId"].unique()
@@ -20,12 +21,19 @@ def get_ages(data):
     date_of_birth_sanitized = pd.to_datetime(
         date_of_birth, format="%m/%d/%Y", errors="coerce"
     )
-    age = pd.to_datetime("now") - date_of_birth_sanitized
+    year = pd.to_datetime(data["year"], format="%Y")
+    age = (year - date_of_birth_sanitized).map(
+        lambda time_delta: int(time_delta.days // AVARAGE_DAYS_IN_A_YEAR)
+    )
     return age
 
 
 if __name__ == "__main__":
     data = pd.read_csv(Path("nba_stats.csv"))
+    relevant_information = pd.concat(
+        [data["playerId"].to_frame().T, get_ages(data).to_frame().T]
+    ).T
+    print(relevant_information)
     print(unique_ids(data))
     print(number_of_players(data))
     print(get_ages(data))
