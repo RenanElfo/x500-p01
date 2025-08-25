6299495d1f8edf8dc6e0777f9d697040000e0fb0
Elfo <renanmedeirosaguiar@gmail.com>
2025-08-25 18:09:06 -0300

    Added functions to get height and weight

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from pathlib import Path
 import pandas as pd
 
 AVARAGE_DAYS_IN_A_YEAR = 365.2425
+FEET_TO_CENTIMETER = 30.48
+INCHES_TO_CENTIMETER = 2.54
+POUNDS_TO_KILOGRAMS = 0.4535924
 
 def unique_ids(data):
     players_ids = data["playerId"].unique()
@@ -28,12 +31,37 @@ def get_ages(data):
     return age
 
 
+def unique_ages(data):
+    return get_ages(data).unique()
+
+
+def split_height_and_weight(data):
+    def get_height(htwt):
+        height = htwt.split(",")[0].strip()
+        feet, inches = map(lambda s: s[:1], height.split())
+        centimeters = FEET_TO_CENTIMETER*float(feet) + INCHES_TO_CENTIMETER*float(inches)
+        return centimeters
+    def get_weight(htwt):
+        weight = float(htwt.split(",")[1].strip().split()[0].split()[0])
+        return POUNDS_TO_KILOGRAMS*weight
+    height = data["htwt"].map(get_height).rename("height")
+    weight = data["htwt"].map(get_weight).rename("weight")
+    return (height, weight)
+
+
 if __name__ == "__main__":
     data = pd.read_csv(Path("nba_stats.csv"))
-    relevant_information = pd.concat(
-        [data["playerId"].to_frame().T, get_ages(data).to_frame().T]
-    ).T
-    print(relevant_information)
-    print(unique_ids(data))
-    print(number_of_players(data))
-    print(get_ages(data))
+    data = data[~data["htwt"].isna()]
+    # data = data[data["htwt"].isna()]
+    # relevant_information = pd.concat(
+    #     [data["playerId"].to_frame().T, get_ages(data).to_frame().T]
+    # ).T
+    # print(relevant_information)
+    # print(unique_ids(data))
+    # print(number_of_players(data))
+    # print(get_ages(data))
+    # x = unique_ages(data)
+    # print(x)
+    # x.sort()
+    # print(x)
+    print(split_height_and_weight(data))
